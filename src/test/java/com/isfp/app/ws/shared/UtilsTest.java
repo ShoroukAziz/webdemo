684777baadd6f2ee2f40747e2b4fe5f37569c8cb
shsaied <shsaied@RD-SC-LP-2011.ISFPDOMAIN.com>
2021-08-05 15:58:45 +0200

    implement test for generateUserId method

---
+++ src/test/java/com/isfp/app/ws/shared/UtilsTest.java
@@ -0,0 +1,48 @@
+package com.isfp.app.ws.shared;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class UtilsTest {
+	
+	@Autowired
+	Utils utils;
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testGenerateUserId() {
+		
+		String userId = utils.generateAddressId(30);
+		String userId2 = utils.generateAddressId(30);
+		
+		assertNotNull(userId);
+		assertNotNull(userId2);
+		
+		assertTrue(userId.length() == 30);
+		assertTrue(!userId.equalsIgnoreCase(userId2));
+		
+		
+ 	}
+
+	@Disabled
+	@Test
+	void testGenerateAddressId() {
+		fail("Not yet implemented");
+	}
+
+}
