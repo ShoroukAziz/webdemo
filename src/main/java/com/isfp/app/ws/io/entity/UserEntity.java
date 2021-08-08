b917e8bd68018319486ecad527f96aed664dd010
shsaied <shsaied@RD-SC-LP-2011.ISFPDOMAIN.com>
2021-08-08 16:10:31 +0200

    add @Table annotation

--- src/main/java/com/isfp/app/ws/io/entity/UserEntity.java
+++ src/main/java/com/isfp/app/ws/io/entity/UserEntity.java
@@ -5,15 +5,15 @@ import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
-import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 
-@Entity(name="users")
+@Entity
+@Table(name="users")
 public class UserEntity implements Serializable {
 
 	/**
