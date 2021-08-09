e1d271043e2c478192129460bef7fc7e3edbcb0d
shsaied <shsaied@RD-SC-LP-2011.ISFPDOMAIN.com>
2021-08-09 12:34:10 +0200

    swagger done

--- src/main/java/com/isfp/app/ws/SwaggerConfig.java
+++ src/main/java/com/isfp/app/ws/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.isfp.app.ws;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
+import java.util.HashSet;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,16 +16,42 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
+
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+	
+	
+	  Contact contact = new Contact(
+	            "Shorouk Abdelaziz",
+	            "https://shorouk.dev", 
+	            "contact@shorouk.dev"
+	    );
+	
+	  //TODO: SEARCH
+	 List<VendorExtension> vendorExtensions = new ArrayList<>();
+		
+		ApiInfo apiInfo = new ApiInfo(
+				"Photo app RESTful Web Service documentation",
+				"This pages documents Photo app RESTful Web Service endpoints", 
+				"1.0",
+				"https://shorouk.dev", 
+				contact, 
+				"Apache 2.0",
+				"http://www.apache.org/licenses/LICENSE-2.0", 
+				vendorExtensions);
 
 		@Bean
 		public Docket apiDocket() {
 			
 			Docket docket = new Docket(DocumentationType.SWAGGER_2)
+					.apiInfo(apiInfo)
+					.protocols(new HashSet<>(Arrays.asList("HTTP","HTTPs")))
 					.select()
 					.apis(RequestHandlerSelectors.basePackage("com.isfp.app.ws"))
 					.paths(PathSelectors.any())
